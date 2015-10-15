0a87f2a18ef9022225099b5269c1fe4c8d58316a
Michael J. Calkins <michael.calkins@musicbed.com>
2015-10-15 09:45:22 -0700

    updates to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,15 @@
     @include('partials.publicHeader')
 
     <section class="section-home">
-        <h1>Your team's communication on one platform</h1>
+        <h1>Your Communication<br>On One Platform.</h1>
         <p>Your whole team on one platform, with multiple ways of working with projects.</p>
         <a href="/auth/register" class="btn btn-success btn-lg">Sign Up For Free</a>
-        <a href="#section-features" class="btn btn-primary btn-lg">Learn More</a>
         <div class="site-preview"></div>
     </section>
 
     <section class="section-features">
-        <h2 class="section-feature-heading">Tab Section</h2>
-        <p class="section-feature-description">This is some text inside of a div block.</p>
+        <h2 class="section-feature-heading">Everyone communicates differently</h2>
+        <p class="section-feature-description">We offer several styles of organization to fit your personal preference.</p>
 
         <div class="row">
             <div class="col-sm-12 centered-pills">
@@ -26,9 +25,9 @@
                     <li>
                         <a href="#feature-tasklist" data-toggle="tab">Tasklist</a>
                     </li>
-                    <li>
+                    {{-- <li>
                         <a href="#feature-daily-summary" data-toggle="tab">Daily Summary</a>
-                    </li>
+                    </li> --}}
                     {{-- <li>
                         <a href="#feature-timeline" data-toggle="tab">Timeline</a>
                     </li>
@@ -48,24 +47,26 @@
         <!-- Tab panes -->
         <div class="tab-content">
             <div class="tab-pane fade in active" id="feature-projects">
-                <h3>Pjrojects Heading</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.<br>
-Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.<br>
-Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
-                <img src="http://placehold.it/940x627">
+                <h3>Clear Project Status</h3>
+                <p>
+                    Good for constant updates and clearly communicating where you are with a project.<br>
+                    Allowing a project lead to glance quickly over the size and progress of a project.
+                </p>
+                <img src="/img/feature-projects.png">
             </div>
             <div class="tab-pane fade" id="feature-tasklist">
-                <h3>Tasklist Heading</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.<br>
-Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.<br>
-Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
-                <img src="http://placehold.it/940x627">
+                <h3>Overview Of All Tasks</h3>
+                <p>
+                    Turns your cards into a list of tasks that allows you to sort and filter easily.<br>
+                    Lets you scan quickly over the work that your team is going to be working on.
+                </p>
+                <img src="/img/feature-tasklist.png">
             </div>
             <div class="tab-pane fade" id="feature-daily-summary">
-                <h3>Daiuly Heading</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.<br>
-Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.<br>
-Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+                <h3>Communicate What You Did Today</h3>
+                <p>
+                    Keep other team members and managers in the loop with the details of what you worked on for the day.
+                </p>
                 <img src="http://placehold.it/940x627">
             </div>
         </div>
