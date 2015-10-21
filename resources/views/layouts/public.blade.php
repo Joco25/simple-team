ae6ddeb5337fd91ef91d83fa2e6f93d1d9f25d34
Michael J. Calkins <michael.calkins@musicbed.com>
2015-10-20 22:27:49 -0700

    updated description

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Your favorite way of working with projects and all in one place.">
-    <meta name="author" content="">
+    <meta name="description" content="Simple project management for your team all in one place.">
+    <meta name="author" content="@michaeljcalkins">
 
     <title>simple.team | Project management for your team all in one place.</title>
 
