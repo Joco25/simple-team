f88f23a7ceb91ef263252897a6b2a71f5a36f18c
Michael J. Calkins <michael.calkins@musicbed.com>
2015-11-10 07:54:04 -0800

    udpates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+Bugsnag::notifyError('ErrorType', 'Test Error');
 Route::get('/', 'HomeController@index');
 
 // Authentication routes...
